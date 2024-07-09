ff2c1182683690bb102a8b5c64ee093f8627a6f0
diego.rosazza <diego.rosazza@truextend.com>
2024-07-09 15:47:18 -0400

    Added the base structure, styling and data for the student page

--- src/app/modules/students/students.module.ts
+++ src/app/modules/students/students.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { StudentsPageComponent } from './page/students-page.component';
+import { MatListModule } from '@angular/material/list';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
   declarations: [StudentsPageComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, MatListModule, MatIconModule],
 })
 export class StudentsModule {}
